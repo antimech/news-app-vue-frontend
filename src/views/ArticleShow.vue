06b66671679739ada455f6c6e8a4518909201eca
Artur Gauzer <artur.gauzer@pm.me>
2024-02-14 12:49:47 +0600

    Add padding and border on article show page to article's content for screens wider than 425 CSS pixels

--- src/views/ArticleShow.vue
+++ src/views/ArticleShow.vue
@@ -28,22 +28,37 @@ onMounted(async () => {
       <img :src="article.image_url" alt="">
     </div>
 
-    <p>
-      {{ article.content }}
-    </p>
+    <div class="content">
+      <p>
+        {{ article.content }}
+      </p>
 
-    <div class="article-footer">
-      <div>
-        Author: {{ article.author.name }}
-      </div>
-      <div class="date">
-        Published at: {{ DateTime.fromISO(article.created_at)}}
+      <div class="article-footer">
+        <div>
+          Author: {{ article.author.name }}
+        </div>
+        <div class="date">
+          Published at: {{ DateTime.fromISO(article.created_at)}}
+        </div>
       </div>
     </div>
   </article>
 </template>
 
 <style scoped>
+@media (min-width: 425px) {
+  article .content {
+    border: 1px solid #808080;
+    padding: .5rem;
+  }
+}
+
+@media (min-width: 768px) {
+  article .content {
+    padding: .25rem .75rem;
+  }
+}
+
 h1 {
   text-align: center;
 }
